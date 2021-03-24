98b959976445fb793b41bc8a2113019d73685152
Paul Morris <PaulMorrisCoder@mail.com>
2021-03-24 14:17:47 +0000

    Removed IR send references

--- src/ir-decoder-main.cpp
+++ src/ir-decoder-main.cpp
@@ -3,20 +3,14 @@
 
 int IR_RECEIVE_PIN  = 15;
 
-uint16_t sAddress = 0xA0;
-uint8_t sCommand = 0x5D;
-uint8_t sRepeats = 0;
-
 void setup() {
   Serial.begin(115200);
 
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
-  IrSender.begin(IR_SEND_PIN, ENABLE_LED_FEEDBACK);
 
   Serial.println();
   Serial.println(F(__FILE__ " : " __DATE__ " : " __TIME__));
   Serial.printf("IR Receive pin %i \n", IR_RECEIVE_PIN);
-  Serial.printf("IR Send pin %i \n", (int)IR_SEND_PIN);   // IR_SEND_PIN defined in library
   Serial.println();
 }
 
